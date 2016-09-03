18cde2311dda23cd47bf45561f4e666e67aed246
Soloveik <Soloveik@mail.ru>
2016-09-03 15:12:44 +0300

    first Kalifa Deploy

--- app/controllers/managers_controller.rb
+++ app/controllers/managers_controller.rb
@@ -2,9 +2,9 @@ class ManagersController < ApplicationController
   require "hashie"
 
   def index
-    @vk = VkontakteApi::Client.new(Token.first.token)
-    # @vk.token = Token.first.token
-    data = @vk.users.get.first.to_hash
-    @data = data.inspect
+    # @vk = VkontakteApi::Client.new(Token.first.token)
+    # # @vk.token = Token.first.token
+    # data = @vk.users.get.first.to_hash
+    # @data = data.inspect
   end
 end
